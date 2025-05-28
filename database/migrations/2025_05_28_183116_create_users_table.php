c86d1c7447587204dde8ce1917e69f0611e98d98
morendev <morenodasilvajoao709@gmail.com>
2025-05-28 15:38:39 -0300

    feat: efetua migration para o banco de dados

--- database/migrations/2025_05_28_183116_create_users_table.php
+++ database/migrations/2025_05_28_183116_create_users_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
+            $table->string('email');
+            $table->integer('yearsOld');
+            $table->string('password');
         });
     }
 
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('users');
     }
 };
